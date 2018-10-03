545ec57f15081fd173ac7df4541cac3e59de8211
Peng <pengyuca@gmail.com>
2018-10-03 11:43:49 +0800

    model move

---
+++ Assets/ActorController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActorController : MonoBehaviour {
+
+    public GameObject model;
+    public playerInput pi;
+
+    [SerializeField]
+    private Animator anim;
+    [SerializeField]
+    private Rigidbody rigid;
+    [SerializeField]
+    private Vector3 movingVec;
+
+	// Use this for initialization
+	void Awake () {
+
+        pi = GetComponent<playerInput>();
+        anim = model.GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {  //Time.deltaTime 1/60
+        //print(pi.Dup);
+        anim.SetFloat("forward", pi.Dmag);
+        if(pi.Dmag > 0.1f){
+            model.transform.forward = pi.Dvec;
+        }
+        movingVec = pi.Dmag * model.transform.forward;
+        
+	}
+
+    void FixedUpdate () {
+        rigid.position += movingVec * Time.fixedDeltaTime;
+       
+    }
+}
---
+++ Assets/playerInput.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerInput : MonoBehaviour {
+
+    //variable
+    public string keyUp = "w"; 
+    public string keyDown = "s"; 
+    public string keyLeft = "a";
+    public string keyRight = "d"; 
+
+    public float Dup;
+    public float Dright;
+    public float Dmag;
+    public Vector3 Dvec;
+
+    public bool inputEnable = true; //flag
+
+    private float targetDup;
+    private float targetDright;
+    private float velocityDup;
+    private float velocityDright;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        targetDup = (Input.GetKey(keyUp)?1.0f:0) - (Input.GetKey(keyDown)?1.0f:0);
+        targetDright = (Input.GetKey(keyRight) ? 1.0f : 0) - (Input.GetKey(keyLeft) ? 1.0f : 0);
+
+        if (inputEnable == false) {
+            targetDup = 0;
+            targetDright = 0;
+        }
+
+        Dup = Mathf.SmoothDamp(Dup, targetDup, ref velocityDup, .1f);
+        Dright = Mathf.SmoothDamp(Dright, targetDright, ref velocityDright, .1f);
+        Dmag = Mathf.Sqrt((Dup * Dup) + (Dright * Dright));
+        Dvec = Dright * transform.right + Dup * transform.forward;
+    }
+}
